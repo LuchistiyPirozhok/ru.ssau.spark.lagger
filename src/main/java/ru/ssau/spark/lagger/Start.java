d7ce787e646026b101ac5528b7418b7ba51b35e4
Alexsey <mazaev.alexsey@yandex.ru>
2018-01-05 01:05:08 +0400

    add some src's and etc

--- src/main/java/ru/ssau/spark/lagger/Start.java
+++ src/main/java/ru/ssau/spark/lagger/Start.java
@@ -2,6 +2,11 @@ package ru.ssau.spark.lagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.ssau.spark.lagger.logic.CalculationUtils;
+import ru.ssau.spark.lagger.math.LagerrsFunction;
+import ru.ssau.spark.lagger.math.LagerrsIntegral;
+import ru.ssau.spark.lagger.math.LaggersDerivative;
+import ru.ssau.spark.lagger.math.MathUtils;
 
 
 /**
@@ -10,8 +15,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Start {
     public static void main(String[] args) throws Exception {
+        new LaggersDerivative();
+        new LagerrsFunction();
+        new LagerrsIntegral();
+        new MathUtils();
+        new CalculationUtils();
 
-       SpringApplication.run(Start.class, args);
+        SpringApplication.run(Start.class, args);
 
     }
 }
